9582942ec240eb8cfa69a5ee7a4bad0cc56b5d9a
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-03-07 05:28:39 +0600

    Created Login Page

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const issues = [];
+const issues = [
+    {
+        id: 1,
+        title: "Login button not working",
+        description: "User cannot login after clicking the login button",
+        status: "open"
+    },
+    {
+        id: 2,
+        title: "Page layout broken",
+        description: "Homepage layout breaks on mobile screen",
+        status: "open"
+    }
+];
 
 function renderIssues() {
-    console.log("Render issues here");
-
+    console.log(issues);
 }
 
 renderIssues();
