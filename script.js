86c1b4366ae0815094134ef4dbab296709859109
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-03-07 11:10:03 +0600

    Render issue cards dynamically

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
-const issues = [
-    {
-        id: 1,
-        title: "Login button not working",
-        description: "User cannot login after clicking the login button",
-        status: "open"
-    },
-    {
-        id: 2,
-        title: "Page layout broken",
-        description: "Homepage layout breaks on mobile screen",
-        status: "open"
-    }
-];
 
-function renderIssues() {
-    console.log(issues);
+const API_URL = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+async function loadIssues(filter = "all") {
+    const response =await fetch(API_URL);
+    const data = await response.json();
+
+    let issues = data.data;
+
+    if (filter === "open") {
+        issues =issues.filter(issues => issues.status === "open");
+
+    }
+
+    if (filter === "closed") {
+        issues =issues.filter(issues => issues.status === "closed");
+    }
+
+    renderIssues(issues);
 }
 
-renderIssues();
+function renderIssues(issues) {
+
+    const container = document.getElementById("issuesContainer");
+
+    container.innerHTML = "";
+
+    issues.forEach(issue => {
+
+        const card = document.createElement("div");
+
+        card.className = "bg-white shadow-md rounded p-4 border-t-4";
+
+        card.innerHTML = `
+            <h2 class="font-bold text-lg mb-2">${issue.title}</h2>
+
+            <p class="text-gray-600 text-sm mb-2">
+            ${issue.description}
+            </p>
+
+            <p class="text-sm"><b>Status:</b> ${issue.status}</p>
+            <p class="text-sm"><b>Category:</b> ${issue.category}</p>
+            <p class="text-sm"><b>Author:</b> ${issue.author}</p>
+        `;
+
+        container.appendChild(card);
+
+    });
+
+}
 
 function login() {
+
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
     if (username === "admin" && password === "admin123") {
-        alert("login successful");
+
+        document.querySelector(".bg-white").style.display = "none";
+        document.getElementById("mainPage").classList.remove("hidden");
+
+        loadIssues(); // load issues after login
     }
+
     else {
-        alert("Invalid credentials")
+        alert("Invalid credentials");
     }
 
 }
+
+// LOAD ISSUES WHEN PAGE STARTS
+loadIssues();
